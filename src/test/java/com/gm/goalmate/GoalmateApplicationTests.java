b9735eab7b2afbcf37d681429a090121d0ea7170
aaaawg <divv09@naver.com>
2025-11-20 10:31:18 +0900

    Initial commit

---
+++ src/test/java/com/gm/goalmate/GoalmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.goalmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
