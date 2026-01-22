30f7d5790e19e1108d75bc782b9c1c1c4be44eb2
1gyng <divv09@naver.com>
2026-01-22 17:52:18 +0900

    feat: 목표 상태 자동 갱신(@Scheduled 사용)

--- src/main/java/com/gm/goalmate/domain/goal/GoalRepository.java
+++ src/main/java/com/gm/goalmate/domain/goal/GoalRepository.java
@@ -1,9 +1,21 @@
 package com.gm.goalmate.domain.goal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface GoalRepository extends JpaRepository<Goal, Long> {
     List<Goal> findAllByUserUserId(Long userId);
+
+    @Query(value = "UPDATE Goal g SET g.status = 'TODO' WHERE g.startDate > :today AND g.status != 'TODO'")
+    void updateStatusToTodo(@Param("today") LocalDate today);
+
+    @Query(value = "UPDATE Goal g SET g.status = 'IN_PROGRESS' WHERE g.startDate <= :today AND g.dueDate >= :today AND g.status != 'IN_PROGRESS'")
+    void updateStatusToInProgress(@Param("today") LocalDate today);
+
+    @Query(value = "UPDATE Goal g SET g.status = 'COMPLETED' WHERE g.dueDate < :today AND g.status != 'COMPLETED'")
+    void updateStatusToCompleted(@Param("today") LocalDate today);
 }
--- src/main/java/com/gm/goalmate/service/GoalService.java
+++ src/main/java/com/gm/goalmate/service/GoalService.java
@@ -8,6 +8,7 @@ import com.gm.goalmate.domain.user.UserRepository;
 import com.gm.goalmate.dto.GoalRequest;
 import com.gm.goalmate.dto.GoalResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -68,6 +69,15 @@ public class GoalService {
                         .type(goal.getType())
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * *") // 매일 0시 0분 0초(자정)에 실행
+    public void updateStatus() {
+        LocalDate today = LocalDate.now();
+        goalRepository.updateStatusToTodo(today);
+        goalRepository.updateStatusToInProgress(today);
+        goalRepository.updateStatusToCompleted(today);
     }
 
     private Goal findGoalByGoalId(Long goalNum) {
